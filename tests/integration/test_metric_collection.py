7c52d62eb1fa6f8796871a9fa96558716477c19b
Eduard Tichy <17002066+etichy@users.noreply.github.com>
2023-02-20 10:40:26 +0100

    NH-32170 - Add job metrics (#166)
    Co-authored-by: tomas.panik <tomas.panik@solarwinds.com>

--- tests/integration/test_metric_collection.py
+++ tests/integration/test_metric_collection.py
@@ -6,14 +6,19 @@ from test_utils import retry_until_ok
 
 endpoint = os.getenv("TIMESERIES_MOCK_ENDPOINT", "localhost:8088")
 url = f'http://{endpoint}/metrics.json'
-expected_metric_names = ['scrape_samples_scraped', 'k8s.deployment.condition.available', 'k8s.kube_deployment_status_replicas_available', 'k8s.cluster.nodes', 'k8s.kube_pod_start_time', 'k8s.kube_deployment_status_condition', 'k8s.kube_pod_created', 'k8s.node.status.condition.memorypressure', 'k8s.container_network_transmit_packets_dropped_total', 'k8s.cluster.memory.capacity', 'k8s.cluster.spec.memory.requests', 'k8s.pod.network.receive_packets_dropped', 'k8s.node.status.condition.ready', 'k8s.kube_pod_container_state_started', 'k8s.kube_pod_container_status_ready', 'k8s.cluster.memory.utilization', 'k8s.kube_pod_container_status_terminated', 'k8s.kube_namespace_created', 'k8s.kube_namespace_status_phase', 'k8s.kube_deployment_status_replicas_unavailable', 'k8s.node.fs.iops', 'k8s.node.network.bytes_transmitted', 'k8s.kube_replicaset_created', 'k8s.kube_daemonset_status_desired_number_scheduled', 'k8s.kube_pod_status_phase', 'scrape_duration_seconds', 'k8s.kube.replicaset.owner.deployment', 'k8s.node.network.transmit_packets_dropped', 'k8s.pod.containers.running', 'scrape_samples_post_metric_relabeling', 'k8s.kube_daemonset_status_updated_number_scheduled', 'k8s.kube_node_spec_unschedulable', 'k8s.container.status', 'k8s.kube_pod_container_resource_limits', 'k8s.node.network.packets_transmitted', 'k8s.container_network_receive_packets_dropped_total', 'k8s.kube_pod_container_status_waiting', 'k8s.node.memory.capacity', 'k8s.kube_replicaset_owner', 'k8s.container_fs_reads_bytes_total', 'k8s.container_fs_reads_total', 'k8s.pod.fs.reads.rate', 'k8s.kube_node_info', 'k8s.kube_node_status_ready', 'k8s.node.status.condition.networkunavailable', 'k8s.container_cpu_usage_seconds_total', 'k8s.kube_daemonset_status_current_number_scheduled', 'k8s.node.memory.working_set', 'k8s.kube_daemonset_status_number_unavailable', 'k8s.kube.pod.owner.replicaset', 'k8s.pod.memory.working_set', 'k8s.pod.fs.writes.rate', 'k8s.cluster.nodes.ready', 'k8s.container_memory_working_set_bytes', 'k8s.kube_pod_container_status_restarts_total', 'k8s.deployment.condition.progressing',
-                         'k8s.kube_deployment_spec_replicas', 'k8s.kube_node_created', 'k8s.pod.spec.memory.limit', 'k8s.cluster.cpu.utilization', 'k8s.kube_pod_info', 'k8s.pod.network.transmit_packets_dropped', 'k8s.kube_node_status_allocatable', 'k8s.node.memory.allocatable', 'k8s.kube_daemonset_created', 'k8s.cluster.cpu.capacity', 'k8s.node.cpu.capacity', 'k8s.container_spec_cpu_period', 'k8s.container_network_transmit_bytes_total', 'k8s.kube_deployment_status_replicas_ready', 'k8s.node.status.condition.diskpressure', 'k8s.node.network.bytes_received', 'k8s.kube_deployment_created', 'k8s.kube_deployment_status_replicas', 'k8s.kube_node_status_capacity', 'k8s.kube_daemonset_status_number_available', 'k8s.pod.containers', 'k8s.kube_pod_owner', 'k8s.node.cpu.usage.seconds.rate', 'up', 'k8s.kube_deployment_spec_paused', 'k8s.container_network_receive_bytes_total', 'k8s.kube_deployment_labels', 'k8s.kube_daemonset_status_number_ready', 'k8s.container.spec.memory.requests', 'k8s.container_network_transmit_packets_total', 'k8s.node.cpu.allocatable', 'k8s.cluster.memory.allocatable', 'k8s.kube_daemonset_status_number_misscheduled', 'k8s.kube_pod_container_info', 'k8s.container_fs_writes_bytes_total', 'k8s.pod.fs.iops', 'k8s.node.pods', 'k8s.kube_pod_container_status_running', 'k8s.node.network.receive_packets_dropped', 'k8s.node.status.condition.pidpressure', 'k8s.pod.network.bytes_transmitted', 'k8s.pod.spec.memory.requests', 'k8s.cluster.pods.running', 'k8s.cluster.nodes.ready.avg', 'k8s.pod.network.packets_transmitted', 'k8s.kube_daemonset_labels', 'k8s.pod.fs.usage.bytes', 'k8s.container_network_receive_packets_total', 'k8s.kube_node_status_condition', 'k8s.cluster.cpu.allocatable', 'k8s.container_fs_usage_bytes', 'k8s.cluster.pods', 'k8s.container_spec_memory_limit_bytes', 'k8s.kube_pod_status_ready', 'k8s.pod.network.bytes_received', 'scrape_series_added', 'k8s.pod.network.packets_received', 'k8s.node.network.packets_received', 'k8s.pod.cpu.usage.seconds.rate', 'k8s.container_fs_writes_total', 'k8s.kube_pod_container_resource_requests', 'k8s.kube_deployment_status_replicas_updated', 'k8s.node.fs.usage']
+
+with open('expected_metric_names.txt', "r", newline='\n') as file_with_expected_metric_names:
+    expected_metric_names = file_with_expected_metric_names.read().splitlines()
 
 def test_metric_names_generated():
-    retry_until_ok(url, assert_test_metric_names_found, print_failure_metric_names)
+    retry_until_ok(url, assert_test_metric_names_found,
+                   print_failure_metric_names)
+
 
 def test_metric_line_count_generated():
-    retry_until_ok(url, assert_test_metrics_line_length_match, print_failure_line_count)
+    retry_until_ok(url, assert_test_metrics_line_length_match,
+                   print_failure_line_count)
+
 
 def assert_test_metric_names_found(content):
     lines = content.splitlines()
@@ -21,20 +26,31 @@ def assert_test_metric_names_found(content):
     merged_json = merge_jsons(metrics)
 
     metric_names = get_unique_metric_names(merged_json)
+    if (len(metric_names) == 0):
+        return False
+
+    write_actual = os.getenv("WRITE_ACTUAL", "False")
+    if write_actual == "True":
+        with open("expected_metric_names.txt", "w", newline='\n') as f:
+            f.write("\n".join(sorted(metric_names)))
+
     metric_matches = False
     if all(name in metric_names for name in expected_metric_names):
         print("All specific metric names are found in the response.")
         metric_matches = True
     else:
-        missing_metric_names = [name in metric_names for name in expected_metric_names]
+        missing_metric_names = [
+            name in metric_names for name in expected_metric_names]
         print('Some specific metric names are not found in the response')
         print(f'Missing metrics: {missing_metric_names}')
-    
+
     return metric_matches
+
 
 def print_failure_metric_names(content):
     print(f'Failed to find some of expected metric names')
     print(expected_metric_names)
+
 
 def assert_test_metrics_line_length_match(content):
     with open('expected_output.json', "r", newline='\n') as file_with_expected:
@@ -47,7 +63,7 @@ def assert_test_metrics_line_length_match(content):
     actual_json = json.dumps(merged_json, sort_keys=True, indent=2)
     expected_json = json.dumps(expected_json_raw, sort_keys=True, indent=2)
 
-    write_actual = os.getenv("WRITE_ACTUAL_JSON", "False")
+    write_actual = os.getenv("WRITE_ACTUAL", "False")
     if write_actual == "True":
         with open("actual.json", "w", newline='\n') as f:
             f.write(actual_json)
@@ -63,6 +79,7 @@ def assert_test_metrics_line_length_match(content):
 
     return length_matches
 
+
 def print_failure_line_count(content):
     lines = content.splitlines()
     metrics = [json.loads(line) for line in lines]
@@ -70,6 +87,7 @@ def print_failure_line_count(content):
     actual_json = json.dumps(merged_json, sort_keys=True, indent=2)
     print('Actual json:')
     print(actual_json)
+
 
 def merge_jsons(jsons):
     result = {}
--- utils/cleanup_mocked_prometheus_response.py
+++ utils/cleanup_mocked_prometheus_response.py
@@ -1,6 +1,8 @@
 # This script is supposed to simply preparation of mocked data for integraiton test, to keep there minimum set of datapoints
 import re
 import requests
+import os
+from dotenv import load_dotenv
 
 now = 1675856675021 # to have unified timestamp in all datapoints
 
@@ -25,6 +27,8 @@ def replace_values(lines):
         ('pod', 'test-pod'),
         ('service', 'test-service'),
         ('container', 'test-container'),
+        ('job', 'test-job'),
+        ('job_name', 'test-job-name'),
     ]
     for line in lines:
         for replacement in replacements:
@@ -34,11 +38,7 @@ def replace_values(lines):
     return result
 
 def replace_line(line, attribute, new_value):
-    match = re.search(f'{attribute}="([^"]+)"', line)
-    if match:
-        val = match.group(1)
-        return line.replace(val, new_value)
-    return line
+    return re.sub(fr'{attribute}="[^"]+"', f'{attribute}="{new_value}"', line)
 
 indexers = []
 def limit_items_taken(name, target_number):
@@ -51,11 +51,25 @@ def limit_items_taken(name, target_number):
     indexers.append(list((name, 1)))
     return False
 
+def limit_metric_taken(line):
+    match = re.search(r'^([^\s\{\#]+)', line)
+    if match:
+        metric_name = match.group(1)
+        return limit_items_taken(metric_name, 1)
+    else:
+        return False
+
 pod = 'swi-k8s-otel-collector-swo-k8s-collector-metrics'
 daemonset = 'swi-k8s-otel-collector-swo-k8s-collector-logs'
 namespace = 'prometheus-system'
-prometheushost = '<FILL HERE>'
-url = f'https://{prometheushost}/federate?match%5B%5D=container_cpu_usage_seconds_total&match%5B%5D=container_spec_cpu_quota&match%5B%5D=container_spec_cpu_period&match%5B%5D=container_memory_working_set_bytes&match%5B%5D=container_spec_memory_limit_bytes&match%5B%5D=container_cpu_cfs_throttled_periods_total&match%5B%5D=container_cpu_cfs_periods_total&match%5B%5D=container_fs_reads_total&match%5B%5D=container_fs_writes_total&match%5B%5D=container_fs_reads_bytes_total&match%5B%5D=container_fs_writes_bytes_total&match%5B%5D=container_fs_usage_bytes&match%5B%5D=container_network_receive_bytes_total&match%5B%5D=container_network_transmit_bytes_total&match%5B%5D=container_network_receive_packets_total&match%5B%5D=container_network_transmit_packets_total&match%5B%5D=container_network_receive_packets_dropped_total&match%5B%5D=container_network_transmit_packets_dropped_total&match%5B%5D=kube_deployment_created&match%5B%5D=kube_daemonset_created&match%5B%5D=kube_namespace_created&match%5B%5D=kube_node_info&match%5B%5D=kube_node_created&match%5B%5D=kube_node_status_capacity&match%5B%5D=kube_node_status_condition&match%5B%5D=kube_pod_created&match%5B%5D=kube_pod_info&match%5B%5D=kube_pod_owner&match%5B%5D=kube_pod_completion_time&match%5B%5D=kube_pod_status_phase&match%5B%5D=kube_pod_status_ready&match%5B%5D=kube_pod_status_reason&match%5B%5D=kube_pod_start_time&match%5B%5D=%7B__name__%3D~%22kube_pod_container_.%2A%22%7D&match%5B%5D=kube_namespace_status_phase&match%5B%5D=kube_deployment_labels&match%5B%5D=kube_deployment_spec_replicas&match%5B%5D=kube_deployment_spec_paused&match%5B%5D=kube_deployment_status_replicas&match%5B%5D=kube_deployment_status_replicas_ready&match%5B%5D=kube_deployment_status_replicas_available&match%5B%5D=kube_deployment_status_replicas_updated&match%5B%5D=kube_deployment_status_replicas_unavailable&match%5B%5D=kube_deployment_status_condition&match%5B%5D=kube_replicaset_owner&match%5B%5D=kube_replicaset_created&match%5B%5D=kube_statefulset_labels&match%5B%5D=kube_statefulset_replicas&match%5B%5D=kube_statefulset_status_replicas_ready&match%5B%5D=kube_statefulset_status_replicas_current&match%5B%5D=kube_statefulset_status_replicas_updated&match%5B%5D=kube_statefulset_created&match%5B%5D=kube_daemonset_labels&match%5B%5D=kube_daemonset_status_current_number_scheduled&match%5B%5D=kube_daemonset_status_desired_number_scheduled&match%5B%5D=kube_daemonset_status_updated_number_scheduled&match%5B%5D=kube_daemonset_status_number_available&match%5B%5D=kube_daemonset_status_number_misscheduled&match%5B%5D=kube_daemonset_status_number_ready&match%5B%5D=kube_daemonset_status_number_unavailable&match%5B%5D=kube_resourcequota&match%5B%5D=kube_node_status_allocatable&match%5B%5D=kube_node_spec_unschedulable&match%5B%5D=apiserver_request_total'
+
+load_dotenv()
+prometheushost = os.environ.get('PROMETHEUS_HOST')
+if prometheushost is None:
+    print('PROMETHEUS_HOST is not set')
+    exit(1)
+
+url = f'https://{prometheushost}/federate?match%5B%5D=container_cpu_usage_seconds_total&match%5B%5D=container_spec_cpu_quota&match%5B%5D=container_spec_cpu_period&match%5B%5D=container_memory_working_set_bytes&match%5B%5D=container_spec_memory_limit_bytes&match%5B%5D=container_cpu_cfs_throttled_periods_total&match%5B%5D=container_cpu_cfs_periods_total&match%5B%5D=container_fs_reads_total&match%5B%5D=container_fs_writes_total&match%5B%5D=container_fs_reads_bytes_total&match%5B%5D=container_fs_writes_bytes_total&match%5B%5D=container_fs_usage_bytes&match%5B%5D=container_network_receive_bytes_total&match%5B%5D=container_network_transmit_bytes_total&match%5B%5D=container_network_receive_packets_total&match%5B%5D=container_network_transmit_packets_total&match%5B%5D=container_network_receive_packets_dropped_total&match%5B%5D=container_network_transmit_packets_dropped_total&match%5B%5D=kube_deployment_created&match%5B%5D=kube_daemonset_created&match%5B%5D=kube_namespace_created&match%5B%5D=kube_node_info&match%5B%5D=kube_node_created&match%5B%5D=kube_node_status_capacity&match%5B%5D=kube_node_status_condition&match%5B%5D=kube_pod_created&match%5B%5D=kube_pod_info&match%5B%5D=kube_pod_owner&match%5B%5D=kube_pod_completion_time&match%5B%5D=kube_pod_status_phase&match%5B%5D=kube_pod_status_ready&match%5B%5D=kube_pod_status_reason&match%5B%5D=kube_pod_start_time&match%5B%5D=%7B__name__%3D~%22kube_pod_container_.%2A%22%7D&match%5B%5D=kube_namespace_status_phase&match%5B%5D=kube_deployment_labels&match%5B%5D=kube_deployment_spec_replicas&match%5B%5D=kube_deployment_spec_paused&match%5B%5D=kube_deployment_status_replicas&match%5B%5D=kube_deployment_status_replicas_ready&match%5B%5D=kube_deployment_status_replicas_available&match%5B%5D=kube_deployment_status_replicas_updated&match%5B%5D=kube_deployment_status_replicas_unavailable&match%5B%5D=kube_deployment_status_condition&match%5B%5D=kube_replicaset_owner&match%5B%5D=kube_replicaset_created&match%5B%5D=kube_statefulset_labels&match%5B%5D=kube_statefulset_replicas&match%5B%5D=kube_statefulset_status_replicas_ready&match%5B%5D=kube_statefulset_status_replicas_current&match%5B%5D=kube_statefulset_status_replicas_updated&match%5B%5D=kube_statefulset_created&match%5B%5D=kube_daemonset_labels&match%5B%5D=kube_daemonset_status_current_number_scheduled&match%5B%5D=kube_daemonset_status_desired_number_scheduled&match%5B%5D=kube_daemonset_status_updated_number_scheduled&match%5B%5D=kube_daemonset_status_number_available&match%5B%5D=kube_daemonset_status_number_misscheduled&match%5B%5D=kube_daemonset_status_number_ready&match%5B%5D=kube_daemonset_status_number_unavailable&match%5B%5D=kube_resourcequota&match%5B%5D=kube_node_status_allocatable&match%5B%5D=kube_node_spec_unschedulable&match%5B%5D=apiserver_request_total&match%5B%5D=kube_job_info&match%5B%5D=kube_job_owner&match%5B%5D=kube_job_created&match%5B%5D=kube_job_complete&match%5B%5D=kube_job_failed&match%5B%5D=kube_job_status_active&match%5B%5D=kube_job_status_succeeded&match%5B%5D=kube_job_status_failed&match%5B%5D=kube_job_status_start_time&match%5B%5D=kube_job_status_completion_time&match%5B%5D=kube_job_spec_completions'
 response = requests.get(url)
 if response.status_code == 200:
     print("Successfully downloaded!")
@@ -64,19 +78,9 @@ if response.status_code == 200:
     apiserver_request_count = 0
     result = [line for line in lines
               if (line.startswith("#")
+                  or limit_metric_taken(line) # Take at least one metric for each metric name
                   or (('id="/kubepods/burstable"' in line or 'id="/kubepods"' in line or 'id="/"' in line) and (f'node="{node}"' in line))
                   or ((line.startswith("kube_node_")) and (f'node="{node}"' in line))
-                  or ((line.startswith("apiserver_request_total")) and limit_items_taken("apiserver_request_total", 1))
-                  or ((line.startswith("container_cpu_cfs_periods_total")) and limit_items_taken("container_cpu_cfs_periods_total", 1))
-                  or ((line.startswith("container_cpu_cfs_throttled_periods_total")) and limit_items_taken("container_cpu_cfs_throttled_periods_total", 1))
-                  or ((line.startswith("container_spec_cpu_quota")) and limit_items_taken("container_spec_cpu_quota", 1))
-                  or ((line.startswith("kube_resourcequota")) and limit_items_taken("kube_resourcequota", 1))
-                  or ((line.startswith("kube_statefulset_created")) and limit_items_taken("kube_statefulset_created", 1))
-                  or ((line.startswith("kube_statefulset_labels")) and limit_items_taken("kube_statefulset_labels", 1))
-                  or ((line.startswith("kube_statefulset_replicas")) and limit_items_taken("kube_statefulset_replicas", 1))
-                  or ((line.startswith("kube_statefulset_status_replicas_current")) and limit_items_taken("kube_statefulset_status_replicas_current", 1))
-                  or ((line.startswith("kube_statefulset_status_replicas_ready")) and limit_items_taken("kube_statefulset_status_replicas_ready", 1))
-                  or ((line.startswith("kube_statefulset_status_replicas_updated")) and limit_items_taken("kube_statefulset_status_replicas_updated", 1))
                   or ((f'pod="{pod}' in line or f'deployment="{pod}"' in line or f'replicaset="{pod}' in line or f'daemonset="{daemonset}"' in line) and f'namespace="{namespace}"' in line)
                   or ((line.startswith("kube_namespace_")) and (f'namespace="{namespace}"' in line))
                   )]
