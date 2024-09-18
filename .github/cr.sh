8635683625b913b86b533e3d3409d7b5e891dcd6
Jaroslav Fedor <jaroslav.fedor@solarwinds.com>
2024-09-18 08:41:45 +0200

    Generate changelog to release

--- .github/cr.sh
+++ .github/cr.sh
@@ -12,23 +12,49 @@ main() {
 
     rm -rf .cr-index
     mkdir -p .cr-index
+    RELEASE_NAME=$(yq -e '.name + "-" + .version' deploy/helm/Chart.yaml)
+    NEW_TAG=$RELEASE_NAME
+    
+    # Generate release notes 
+    if [[ "$RELEASE_NAME" == *"alpha"* ]]; then
+        echo "Handling alpha release: $RELEASE_NAME"
+        PREVIOUS_TAG=$(git tag --sort=version:refname | grep alpha | grep -B1 "^swo-k8s-collector" | tail -n 1)
+        LATEST_RELEASE=false
+    else
+        echo "Handling standard release: $RELEASE_NAME"
+        PREVIOUS_TAG=$(git tag --sort=version:refname | grep -v alpha | grep -B1 "^swo-k8s-collector" | tail -n 1)
+        LATEST_RELEASE=true
+    fi
+    
+    GIT_PATH="deploy"
+    echo "# Changed:" > deploy/helm/release-notes.md
+    git log "$PREVIOUS_TAG"..HEAD --pretty=format:"%s" -- $GIT_PATH | grep -v Merge | awk '{print "* " $0}' >> deploy/helm/release-notes.md
+    echo "" >> deploy/helm/release-notes.md
+    echo "**Full Changelog**: https://github.com/solarwinds/swi-k8s-opentelemetry-collector/compare/$PREVIOUS_TAG...$NEW_TAG" >> deploy/helm/release-notes.md
+    echo "Release notes:"
+    cat deploy/helm/release-notes.md
+
 
     echo "Packaging chart ..."
     cr package "deploy/helm"
+    
+    # Find the .tgz file and extract the release name
+    RELEASE_FILE=$(find .cr-release-packages -name '*.tgz')
+    
 
+    echo "Release file: $RELEASE_FILE"
+
+  
+    
     echo 'Releasing chart...'
-    cr upload -c "$(git rev-parse HEAD)"
+    cr upload -c "$(git rev-parse HEAD)" --release-notes-file=release-notes.md --make-release-latest=$LATEST_RELEASE
 
+    
     echo 'Updating chart repo index...'
     cr index
 
-    # Find the .tgz file and extract the release name
-    RELEASE_FILE=$(find .cr-release-packages -name '*.tgz')
-    RELEASE_NAME=$(basename "$RELEASE_FILE" .tgz)
 
-    echo "Release file: $RELEASE_FILE"
-    echo "Release name: $RELEASE_NAME"
-
+    exit 0
     echo 'Pushing update...'
     push_files "$RELEASE_NAME"
 
