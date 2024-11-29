22f64e1303fe8e27e2785bdd2de49bb1ff889708
Peter Stranak <peter.stranak@solarwinds.com>
2024-11-29 13:09:04 +0100

    Adjust integration tests for events

--- tests/integration/test_events_collection.py
+++ tests/integration/test_events_collection.py
@@ -1,6 +1,6 @@
 import pytest
 import os
-from test_utils import get_all_bodies_for_all_sent_content, get_all_resources_for_all_sent_content, has_attribute_with_key_and_value, retry_until_ok, run_shell_command
+from test_utils import get_all_bodies_for_all_sent_content, retry_until_ok, run_shell_command
 
 endpoint = os.getenv("TIMESERIES_MOCK_ENDPOINT", "localhost:8088")
 url = f'http://{endpoint}/events.json'
@@ -16,9 +16,6 @@ def teardown_function():
 def test_events_generated():
     retry_until_ok(url, assert_test_event_found, print_failure)
 
-def test_events_has_labels():
-    retry_until_ok(url, assert_test_event_label_found, print_labels_failure)
-
 def assert_test_event_found(content):
     raw_bodies = get_all_bodies_for_all_sent_content(content)
     test_event_found = any(expected_event in body for body in raw_bodies)
@@ -29,45 +26,3 @@ def print_failure(content):
     print(f'Failed to find "{expected_event}"')
     print('Sent events:')
     print(raw_bodies)
-
-def assert_test_event_label_found(content):
-    raw_bodies = get_all_resources_for_all_sent_content(content)
-    resource = find_resource_with_specific_body(raw_bodies, expected_event)
-    print(resource)
-
-    if resource is not None:
-        return (has_attribute_with_key_and_value(resource, "k8s.pod.labels.test-label", "test-value") and
-                does_not_have_attribute_with_key(resource, "k8s.pod.annotations.test-annotation"))
-    else:
-        print("Resource not found.")
-        return False
-
-def print_labels_failure(content):
-    raw_bodies = get_all_resources_for_all_sent_content(content)
-    print(f'Failed to find "{expected_event}"')
-    print('Sent events:')
-    print(raw_bodies)
-
-def find_resource_with_specific_body(raw_bodies, target_body):
-    for inner_list in raw_bodies:
-        for obj in inner_list:
-            scope_logs = obj.get("scopeLogs", [])
-            for scope_log in scope_logs:
-                log_records = scope_log.get("logRecords", [])
-                for log_record in log_records:
-                    body = log_record.get("body", {}).get("stringValue", "")
-                    if target_body in body:
-                        return obj["resource"]
-
-    return None
-
-def does_not_have_attribute_with_key(resource, target_key):
-    attributes = resource.get("attributes", [])
-    for attribute in attributes:
-        key = attribute.get("key", "")
-        if key == target_key:
-            print(f"Resource has attribute with key '{target_key}'.")
-            return False
-
-    print(f"Resource does not have attribute with key '{target_key}'.")
-    return True
