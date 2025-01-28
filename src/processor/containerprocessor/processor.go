d7879351d229617f9dc02d755c0bb904d3f72250
janatrelakova <jana.trelakova@solarwinds.com>
2025-01-28 15:38:20 +0100

    Improve log messages and return values in the processor

--- src/processor/containerprocessor/processor.go
+++ src/processor/containerprocessor/processor.go
@@ -15,38 +15,28 @@ type containerprocessor struct {
 	logger            *zap.Logger
 }
 
-type Container struct {
-	Name               string
-	ContainerId        string
-	State              string
-	IsInitContainer    bool
-	IsSidecarContainer bool
-	Timestamp          string
-}
-
 func (cp *containerprocessor) processLogs(_ context.Context, ld plog.Logs) (plog.Logs, error) {
 	resourceLogs := ld.ResourceLogs()
-	containers := make([]Container, 0)
 
 	manifests, err := cp.extractPodManifests(resourceLogs)
 	if err != nil {
-		cp.logger.Warn("Failed to parse manifest for pod log record")
-		return ld, err
+		cp.logger.Warn("Failed to parse manifests")
+		return plog.NewLogs(), err
 	}
 
 	if len(manifests) == 0 {
-		cp.logger.Debug("No manifests found")
 		return ld, nil
 	}
 
-	for _, m := range manifests {
-		containers = append(containers, m.extractContainers(cp.logger)...)
-	}
+	rl := NewContainerResourceLogs()
+	rl.CopyTo(ld.ResourceLogs().AppendEmpty())
+	lrs := rl.ScopeLogs().At(0).LogRecords()
 
-	newResourceLogs := ld.ResourceLogs().AppendEmpty()
-	newResource := newResourceLogs.Resource()
-	newResource.Attributes().PutStr("sw.k8s.log.type", "manifest")
-	addContainerResourceLog(newResourceLogs, containers)
+	for _, m := range manifests {
+		cp.logger.Debug("Processing manifest", zap.String("pod-name", m.Metadata.PodName))
+		containers := transformManifestToContainerLogs(m)
+		containers.MoveAndAppendTo(lrs)
+	}
 
 	return ld, nil
 }
@@ -85,33 +75,17 @@ func (cp *containerprocessor) extractPodManifests(resourceLogs plog.ResourceLogs
 	return manifests, nil
 }
 
-func addContainerResourceLog(rl plog.ResourceLogs, containers []Container) {
-	newScopeLogs := rl.ScopeLogs().AppendEmpty()
-	for c := range containers {
-		lr := newScopeLogs.LogRecords().AppendEmpty()
-		lr.Attributes().PutStr("k8s.pod.container", containers[c].Name)
-		lr.Attributes().PutStr("k8s.pod.container.id", containers[c].ContainerId)
-		lr.Attributes().PutStr("k8s.pod.container.state", containers[c].State)
-		lr.Attributes().PutStr("k8s.pod.container.timestamp", containers[c].Timestamp)
-		lr.Attributes().PutStr("k8s.pod.container.isInitContainer", "false")
-		lr.Attributes().PutStr("k8s.pod.container.isSidecarContainer", "false")
-	}
-}
-
 func isPodLog(attributes pcommon.Map) bool {
 	kind, _ := attributes.Get("k8s.object.kind")
 	return kind.Str() == "Pod"
 }
 
-func (cp *containerprocessor) logResourceAttributes(rl plog.ResourceLogs) {
-	j, _ := json.Marshal(rl)
-	cp.logger.Info("Resource logs", zap.String("resource-logs", string(j)))
-}
-
 func (cp *containerprocessor) Start(_ context.Context, _ component.Host) error {
+	cp.logger.Info("Starting container processor")
 	return nil
 }
 
 func (cp *containerprocessor) Shutdown(_ context.Context) error {
+	cp.logger.Info("Shutting down container processor")
 	return nil
 }
