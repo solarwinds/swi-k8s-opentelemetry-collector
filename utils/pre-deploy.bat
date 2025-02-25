bb80279b26d1f157d0f4a4dec969d792903c6a8c
Tomas Panik <tomas.panik@solarwinds.com>
2025-02-25 15:49:27 +0100

    Fixing build on CI, shortening local release name
    Making sure that cleanup is done properly

--- utils/pre-deploy.bat
+++ utils/pre-deploy.bat
@@ -16,3 +16,12 @@ kubectl patch crd/opentelemetrycollectors.opentelemetry.io -p "{\"metadata\":{\"
 :: Delete all resources in the test-namespace
 echo Deleting all resources in the test-namespace
 kubectl delete all --all -n test-namespace
+
+:: Those resources are not deleted by the previous command
+kubectl delete secrets --all -n test-namespace
+kubectl delete configmaps --all -n test-namespace
+kubectl delete persistentvolumeclaims --all -n test-namespace
+kubectl delete serviceaccounts --all -n test-namespace
+kubectl delete roles --all -n test-namespace
+kubectl delete rolebindings --all -n test-namespace
+kubectl delete networkpolicies --all -n test-namespace
